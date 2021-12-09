9e0d28dfbfedb943b3d8b3264178e4512a2fda63
Alvin Le <alvinhle@cpp.edu>
2021-12-08 16:38:09 -0800

    Step 135 complete, almost there!

--- kilo.c
+++ kilo.c
@@ -179,6 +179,19 @@ int editorRowCxToRx(erow *row, int cx) {
   }
   return rx;
 }
+
+int editorRowRxToCx(erow *row, int rx) {
+  int cur_rx = 0;
+  int cx;
+  for (cx = 0; cx < row->size; cx++) {
+    if (row->chars[cx] == '\t')
+      cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+    cur_rx++;
+    if (cur_rx > rx) return cx;
+  }
+  return cx;
+}
+
 void editorUpdateRow(erow *row) {
   int tabs = 0;
   int j;
@@ -358,6 +371,24 @@ void editorSave() {
   }
   free(buf);
   editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
+}
+
+/*** find ***/
+void editorFind() {
+  char *query = editorPrompt("Search: %s (ESC to cancel)");
+  if (query == NULL) return;
+  int i;
+  for (i = 0; i < E.numrows; i++) {
+    erow *row = &E.row[i];
+    char *match = strstr(row->render, query);
+    if (match) {
+      E.cy = i;
+      E.cx = editorRowRxToCx(row, match - row->render);
+      E.rowoff = E.numrows;
+      break;
+    }
+  }
+  free(query);
 }
 
 /*** append buffer ***/
@@ -592,7 +623,9 @@ void editorProcessKeypress() {
       if (E.cy < E.numrows)
         E.cx = E.row[E.cy].size;
       break;
-
+    case CTRL_KEY('f'):
+      editorFind();
+      break;
     case BACKSPACE:
     case CTRL_KEY('h'):
     case DEL_KEY:
@@ -631,6 +664,7 @@ void editorProcessKeypress() {
   quit_times = KILO_QUIT_TIMES;
 }
 
+
 /*** init ***/
 void initEditor() {
   E.cx = 0;
@@ -655,8 +689,8 @@ int main(int argc, char *argv[]) {
     editorOpen(argv[1]);
   }
 
-  editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
-
+  editorSetStatusMessage(
+    "HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
   while (1) {
     editorRefreshScreen();
     editorProcessKeypress();
